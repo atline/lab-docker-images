e32b49ca4f93fcf82a0da295445e4d69f33f35d7
Larry Shen <larry.shen@nxp.com>
2021-03-22 14:13:18 +0800

    Fix data file path.
    Signed-off-by: Larry Shen <larry.shen@nxp.com>

--- lava-docker-slave/setup.py
+++ lava-docker-slave/setup.py
@@ -1,9 +1,17 @@
+import os
+import sys
+from distutils.sysconfig import get_python_lib
 from setuptools import setup, find_packages
 
+
 name="nxp_ls"
+relative_site_packages = get_python_lib().split(sys.prefix + os.sep)[1]
+data_files_relative_path = os.path.join(relative_site_packages, name)
+
+
 setup(
     name=name,
-    version="1.0.0",
+    version="1.1.0",
     author="Larry Shen",
     author_email="larry.shen@nxp.com",
     license="MIT",
@@ -22,5 +30,5 @@ setup(
             'lava_docker_slave = nxp_ls:main',
         ]
     },
-    data_files=[('/'+name, ['lava_docker_slave'])],
+    data_files=[(data_files_relative_path, ['lava_docker_slave'])],
 )
