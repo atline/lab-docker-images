ea186e3588ee8a5d9c18d492fb8ab8aac3bc8b58
Larry Shen <larry.shen@nxp.com>
2021-03-25 22:11:13 +0800

    Simple dockerfile.
    Signed-off-by: Larry Shen <larry.shen@nxp.com>

--- lava-docker-slave/customized.sh
+++ lava-docker-slave/customized.sh
@@ -4,3 +4,4 @@ service tftpd-hpa start
 rpcbind
 service nfs-kernel-server start
 update-ca-certificates
+sed -i "s/\"coordinator_hostname\": .*/\"coordinator_hostname\": \"$master\"/g" /etc/lava-coordinator/lava-coordinator.conf
